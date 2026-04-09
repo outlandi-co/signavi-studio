9d87e60d0c1dd1760279a832f450d8039ce7c82f
outlandi-co <outlandico@gmail.com>
2026-04-09 15:10:09 -0700

    fix: wrap app with CartProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,21 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
-import "./index.css" // 🔥 REQUIRED (THIS FIXES EVERYTHING)
+import "./index.css"
 import { Toaster } from "react-hot-toast"
+
+/* 🔥 IMPORT CART PROVIDER */
+import { CartProvider } from "./context/CartContext"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+
+    {/* 🔥 WRAP APP */}
+    <CartProvider>
+      <App />
+    </CartProvider>
+
     <Toaster position="top-right" />
+
   </React.StrictMode>
 )
